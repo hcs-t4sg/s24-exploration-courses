19d0282f55735426fabc03a6dcf1ca46910c5126
Anie Udosen <anieudosen@college.harvard.edu>
2024-03-28 20:31:22 -0400

    Added Widget Feature

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import styles from "./page.module.css";
 import Calendar from "@/components/calendar";
 import Courses from "@/components/courses";
+import Widget from "@/components/widget";
 
 export default async function Home() {
   return (
     <div className={styles.main}>
+      <Widget></Widget>
       <Courses />
       <Calendar />
     </div>
---
+++ src/components/widget.js
@@ -0,0 +1,11 @@
+import styles from "./widget.module.css"
+// this is creating a function in js
+export default function Calendar(){
+    return (
+        <>
+        <p className={styles.hourText}>This is your course calendar </p>
+
+
+        </>
+    );
+}
