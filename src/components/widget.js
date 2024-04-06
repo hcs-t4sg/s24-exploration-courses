e91f37d51bbf5a0b287304710aa6bc167c34b55b
Anie Udosen <anieudosen@college.harvard.edu>
2024-04-06 13:36:57 -0400

    Changes made

--- src/components/widget.js
+++ src/components/widget.js
@@ -1,11 +1,10 @@
 import styles from "./widget.module.css"
-// this is creating a function in js
+// this is creating a function in js change change
 export default function Calendar(){
     return (
         <>
         <p className={styles.hourText}>This is your course calendar </p>
-
-
+    
         </>
     );
 }
